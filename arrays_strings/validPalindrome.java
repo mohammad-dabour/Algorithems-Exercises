99d12830f47e82d654b67f2f064232be502a43dc
Mohammad dabour <37874948+mohammad-dabour@users.noreply.github.com>
2022-03-30 03:46:23 -0500

    Update validPalindrome.java

--- arrays_strings/validPalindrome.java
+++ arrays_strings/validPalindrome.java
@@ -4,7 +4,7 @@ public class validPalindrome {
 
     
     
-    public static boolean isPalindrome(String s) {
+    private static boolean isPalindrome(String s) {
      
      
         for( int i = 0, j = s.length() - 1; i < j; i++, j--){
